db2be9fade272e06e420541160c53416d01eed58
Bren Whyte <bwhyte@vistaprint.com>
2020-10-25 15:41:25 +0000

    Adding aws_acm_certificate

--- alb.tf
+++ alb.tf
@@ -1,8 +1,19 @@
+resource "aws_acm_certificate" "cxflow" {
+  domain_name       = "cxflow.krypton.vpsvc.com"
+  validation_method = "DNS"
+}
+
 resource "aws_lb" "ecs-production" {
   name               = "ecs-production"
-  subnets            = module.vpc.public_subnets.ids
+  subnets            = module.vpc.public_subnets.*.ids
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb.id]
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
 
   tags = {
     Environment = "production"
@@ -13,8 +24,10 @@ resource "aws_lb" "ecs-production" {
 resource "aws_lb_listener" "https_forward" {
   load_balancer_arn = aws_lb.ecs-production.arn
   port              = 443
-  protocol          = "HTTP"
-
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${aws_acm_certificate.cxflow.arn}"
+  
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.cxflow.arn
