b270674e074088eb5a69dd9c2ca416a96adaca00
Conor Mancone <conor.mancone@cimpress.com>
2020-12-09 14:58:05 -0500

    Small bug

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
   all_tags = merge(var.tags, {
     "Environment" = var.environment,
-    "Terraform Version" = var.terraform_version,
     "Terraform" = "true",
     "Squad" = var.squad
   })
