a7f47fa4c052d8731eb571e6309b3acf820e6354
Bren Whyte <bwhyte@vistaprint.com>
2021-01-05 18:18:42 +0000

    domain_validation_options changed from List to Set

--- acm.tf
+++ acm.tf
@@ -12,20 +12,27 @@ data "aws_route53_zone" "myzone" {
   name = var.dns_zone
 }
 
-resource "aws_route53_record" "route53_record" {
-   depends_on = [
-    aws_acm_certificate.cert
-  ]
 
+resource "aws_route53_record" "route53_record" {
+  depends_on      = [aws_acm_certificate.cert]
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+      type   = "A"
+    }
+  }
   allow_overwrite = true
-  name = tolist(aws_acm_certificate.cert.domain_validation_options)[count.index].resource_record_name
-  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[count.index].resource_record_value]
-  ttl = 60
-  type = tolist(aws_acm_certificate.cert.domain_validation_options)[count.index].resource_record_type
-  zone_id = data.aws_route53_zone.myzone.zone_id
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.public_root_domain.zone_id
+
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.route53_record.fqdn]
+  certificate_arn = aws_acm_certificate.route53_record.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_record: record.fqdn]
 }
