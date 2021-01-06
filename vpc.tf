7f402bca2488c772a682951b0b11906efd866783
Bren Whyte <bwhyte@vistaprint.com>
2021-01-06 17:31:53 +0000

    Needs trailing / in flow-log path

--- vpc.tf
+++ vpc.tf
@@ -25,7 +25,7 @@ module "vpc" {
 }
 
 resource "aws_flow_log" "aws_flowlogs" {
-  log_destination      = "${data.aws_s3_bucket.cimpress-security-flowlogs.arn}/${module.vpc.vpc_id}"
+  log_destination      = "${data.aws_s3_bucket.cimpress-security-flowlogs.arn}/${module.vpc.vpc_id}/"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = module.vpc.vpc_id
