1980adabd6e15ff395c41d9a632c3ed64af2843b
Conor Mancone <conor.mancone@cimpress.com>
2021-01-15 10:43:40 -0500

    Less shenanigans required

--- acm.tf
+++ acm.tf
@@ -35,7 +35,6 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  for_each                = aws_route53_record.cert_validation.fqdn
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation.fqdn[each.key]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation.fqdn : record.fqdn]
 }
