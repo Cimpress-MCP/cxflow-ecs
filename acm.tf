63da9158b78256a9061c7f1b4549b2c1c1523bc5
Conor Mancone <conor.mancone@cimpress.com>
2021-01-15 10:57:45 -0500

    Accidentally renamed the validation cert

--- acm.tf
+++ acm.tf
@@ -34,7 +34,7 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = data.aws_route53_zone.myzone.zone_id
 }
 
-resource "aws_acm_certificate_validation" "cert" {
+resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
