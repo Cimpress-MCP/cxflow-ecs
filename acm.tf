0812a406b468e1ae28827a87e2d606abae17099b
Conor Mancone <conor.mancone@cimpress.com>
2021-01-15 10:37:27 -0500

    One more spot

--- acm.tf
+++ acm.tf
@@ -31,7 +31,7 @@ resource "aws_route53_record" "cert_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = aws_route53_zone.example.zone_id
+  zone_id         = data.aws_route53_zone.example.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
