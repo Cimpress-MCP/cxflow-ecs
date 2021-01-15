15c7fa3bb7b5a0d4ad3fd9a12e20795b0b1873b0
Conor Mancone <conor.mancone@cimpress.com>
2021-01-15 10:51:02 -0500

    This new syntax is not cooperating...

--- acm.tf
+++ acm.tf
@@ -35,6 +35,8 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
+  for_each = aws_route53_record.cert_validation.fqdn
+
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation.fqdn : record.fqdn]
+  validation_record_fqdns = aws_route53_record.cert_validation.fqdn[each.key].fqdn
 }
